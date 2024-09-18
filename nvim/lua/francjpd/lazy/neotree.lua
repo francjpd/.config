f0f6303d6e3298e7abeec407e5b09a52d1e86007
francjpd <francjpd@gmail.com>
2024-09-18 12:34:00 +0200

    add: description to every remap so which key works

--- nvim/lua/francjpd/lazy/neotree.lua
+++ nvim/lua/francjpd/lazy/neotree.lua
@@ -8,6 +8,6 @@ return {
 		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
 	},
 	config = function()
-		vim.keymap.set("n", "<leader>pn", vim.cmd.Neotree)
+		vim.keymap.set("n", "<leader>pn", vim.cmd.Neotree, { desc = "Open neo tree" })
 	end,
 }
--- nvim/lua/francjpd/remap.lua
+++ nvim/lua/francjpd/remap.lua
@@ -1,31 +1,22 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Open the file explorer" })
+vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "Replace selection with default register without overwriting it" })
+
 -- moves selected lines to up and down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- 'n' and 'v' yank the selected text to the system clipboard.
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "yank text to System Clipboard" })
+vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "yank text to System Clipboard" })
 
-
+-- Rapidly scroll down / up
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
--- Define the fold expression for JavaScript, TypeScript, Vue, JSX, and TSX files
--- local foldexpr_js_ts = function()
---   vim.wo.foldmethod = 'expr'
---   vim.wo.foldexpr =
---   'getline(v:lnum)=~\'^\\s*/\\*\\*\'&&getline(v:lnum+1)=~\'\\*/\'?1:getline(v:lnum-1)=~\'^\\s*/\\*\\*\'?0:getline(v:lnum)=~\'\\*/\'?-1:1'
--- end
---
--- -- Keymap to enable folding with the defined fold expression
--- vim.api.nvim_set_keymap('n', '<leader>fc', '', { noremap = true, silent = true, callback = foldexpr_js_ts })
---
--- -- Autocommand to apply the fold expression only for specific file types
--- vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
---   pattern = { '*.js', '*.ts', '*.vue', '*.jsx', '*.tsx' },
---   callback = function()
---     vim.api.nvim_buf_set_keymap(0, 'n', '<leader>fc', '', { noremap = true, silent = true, callback = foldexpr_js_ts })
---   end
--- })
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz", { desc = "Next item in quickfix list" })
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz", { desc = "Previous item in quickfix list" })
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz", { desc = "Next item in location list" })
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz", { desc = "Previous item in location list" })
+
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Substitute word under cursor" })
