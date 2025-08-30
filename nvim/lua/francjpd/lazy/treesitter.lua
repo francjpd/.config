633edb1671a3f1e3554a67e94dc19962985c603b
Fran Poleo <francjpd@gmail.com>
2025-08-30 10:31:05 +0200

    add(config): more config!

--- nvim/lua/francjpd/lazy/treesitter.lua
+++ nvim/lua/francjpd/lazy/treesitter.lua
@@ -17,7 +17,7 @@ return {
 
 			highlight = {
 				-- `false` will disable the whole extension
-				enable = false,
+				enable = true,
 
 				-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
 				-- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
